a1e3cc84960dca67df2e84735952582b376ce505
egapool <egpersent@gmail.com>
2017-02-19 22:17:40 +0900

    nodeにredisのENV与えた

--- src/public/message.php
+++ src/public/message.php
@@ -4,5 +4,7 @@ $message = isset($_POST['msg']) ? $_POST['msg'] : '';
 
 if ( $message !== '' ) {
 	// redis post
-	var_dump($message);
+	$redis = new Redis();
+	$redis->connect($_ENV['REDIS_HOST'],$_ENV['REDIS_PORT']);
+	$redis->publish("chat", $message);
 }
