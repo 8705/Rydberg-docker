e37b86b5290119e0db61bcff4c3c0e7575716402
takobonge <shungo.ishino@gmail.com>
2017-02-19 22:14:41 +0900

    ajaxで受けたmessgeをDBに入れる

--- src/public/message.php
+++ src/public/message.php
@@ -2,7 +2,22 @@
 
 $message = isset($_POST['msg']) ? $_POST['msg'] : '';
 
-if ( $message !== '' ) {
-	// redis post
-	var_dump($message);
+// if ( $message !== '' ) {
+// 	// redis post
+// 	var_dump($message);
+// }
+
+// DB接続
+try {
+	$dbh = new PDO(
+		'mysql:host='.$_ENV['DB_HOST'].';dbname='.$_ENV['DB_NAME'].';charset=utf8',
+		$_ENV['DB_USER'],
+		$_ENV['DB_PASS'],
+		array(PDO::ATTR_EMULATE_PREPARES => false)
+	);
+} catch (PDOException $e) {
+	exit('データベース接続失敗。'.$e->getMessage());
 }
+
+$query = $dbh->prepare("INSERT INTO test (message) VALUES (?)");
+$query->execute([$message]);
